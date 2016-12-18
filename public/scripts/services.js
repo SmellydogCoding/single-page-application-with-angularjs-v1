b208dafe6e842bf4db74620a7a4710abc374ca38
DanielMcNeil <daniel.mcneil@outlook.com>
2016-12-18 14:16:17 -0500

    added data services for api routes

--- public/scripts/services.js
+++ public/scripts/services.js
@@ -1,8 +1,87 @@
 (function() {
   'use strict';
   angular.module('app')
-  
   .service('dataService', function($http) {
     
+    this.getAll = function () {
+      $http.get('http://localhost:5000/api/recipes')
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.getAllCategories = function () {
+      $http.get('http://localhost:5000/api/categories')
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.getAllFoodItems = function () {
+      $http.get('http://localhost:5000/api/fooditems')
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.getCategory = function (category) {
+      $http.get('http://localhost:5000/api/recipes?category=' + category)
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.getID = function (id) {
+      $http.get('http://localhost:5000/api/recipes/' + id)
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.updateID = function (id,data) {
+      $http.put('http://localhost:5000/api/recipes/' + id, data)
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.addRecipe = function (data) {
+      $http.put('http://localhost:5000/api/recipes', data)
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+
+    this.deleteRecipe = function (id,data) {
+      $http.delete('http://localhost:5000/api/recipes' + id, data)
+      .then(function success(response) {
+        return response.data;
+      },
+        function error(response) {
+          console.log('something went wrong');
+        });
+    };
+  
   });
 }());
