ea958c57c9b4f636f49177bf24e065a9d919faba
SmellydogCoding <daniel.mcneil@outlook.com>
2017-01-16 17:47:02 -0500

    jsdoc for factories plus add jsdoc script to package.json

--- public/scripts/directives.js
+++ public/scripts/directives.js
@@ -6,6 +6,16 @@
    * @memberof app
    * @ngdoc directive
    * @name confirm
+   * @example
+    * <div class="confirmWrapper">
+    * <div class="confirm">
+    * <p>
+    * Are you sure that you want to delete<br>the {{vm.toDelete}} recipe?
+    * </p>
+    * <button ng-click="vm.deleteIt()">Yes</button>
+    * <button ng-click="vm.hidden = true">No</button>
+    * </div>
+    *</div>
    */
   .directive('confirm', function() {
     return {
@@ -16,7 +26,16 @@
    * html template for exception errors and http errors
    * @memberof app
    * @ngdoc directive
-   * @name errors
+   * @name errors~
+   * @example
+    * <div class="errors" ng-hide="!errorMessage.message">
+    * <p>{{errorMessage.message}}</p>
+    * <p ng-hide="!errorMessage.reason.data">What happened: {{errorMessage.reason.data}}</p>
+    * <p ng-hide="!errorMessage.reason.message">What happened: {{errorMessage.reason.message}}</p>
+    * <p ng-hide="!errorMessage.reason.status">Why it happened: {{errorMessage.reason.status}} {{errorMessage.reason.statusText}}</p>
+    * <p ng-hide="!errorMessage.reason.stack">Why it happened: {{errorMessage.reason.stack}}</p>
+    * <p>Please wait a few minutes and try again.<br>If you keep getting the same error message, please tell the website owner.</p>
+    * </div>
    */
   .directive('errors', function() {
     return {
